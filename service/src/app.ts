f37980e7472fca2e980f49d66247899114a1f2f3
Behnam Tehrani <behnam777@gmail.com>
2025-03-05 09:20:23 -0500

    Add cors support

--- service/src/app.ts
+++ service/src/app.ts
@@ -3,6 +3,8 @@ dotenv.config();
 
 import express, { Express } from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
+
 
 // Import routes
 import ServiceRoutes from './routes/service.route';
@@ -10,6 +12,8 @@ import ServiceRoutes from './routes/service.route';
 import { readConfiguration } from './utils/config.utils';
 import { errorMiddleware } from './middleware/error.middleware';
 import CustomError from './errors/custom.error';
+import { logger } from './utils/logger.utils';
+import { extractMainDomain } from './utils/domain';
 
 // Read env variables
 readConfiguration();
@@ -17,6 +21,22 @@ readConfiguration();
 // Create the express app
 const app: Express = express();
 app.disable('x-powered-by');
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      const domain = extractMainDomain(origin);
+      logger.info(`Checking origin: ${origin} with domain: ${domain}`);
+      if (process.env.CORS_ALLOWED_ORIGINS?.split(',').includes(domain)) {
+        return callback(null, true);
+      }
+      return callback(new Error('Not allowed by CORS'));
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  })
+);
 
 // Define configurations
 app.use(bodyParser.json());
